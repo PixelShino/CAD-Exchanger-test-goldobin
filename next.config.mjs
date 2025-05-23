69c3f57f0ddb4d60b53e3622ba03f3abf1ecca33
Goldobin <goldobin.dmitry@bk.ru>
2025-05-23 21:13:46 +0300

    Fix deployment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: '.next',
   basePath: '/CAD-Exchanger-test-goldobin',
   images: { unoptimized: true },
   assetPrefix: '/CAD-Exchanger-test-goldobin/',
-  trailingSlash: true,
-  distDir: 'out',
-  experimental: {
-    images: {
-      unoptimized: true,
-    },
-  },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, path: false };
-    return config;
-  },
 }
