c5d2b5e6326daec48fd79881de24273bf39e03e5
Goldobin <goldobin.dmitry@bk.ru>
2025-05-24 13:55:50 +0300

    fix deploy config 6

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: '.next',
-  basePath: '/CAD-Exchanger-test-goldobin',
+  distDir: 'out',
+  // basePath: '/CAD-Exchanger-test-goldobin',
   images: { unoptimized: true },
-  assetPrefix: '/CAD-Exchanger-test-goldobin/',
-}
+
+  // assetPrefix: '/CAD-Exchanger-test-goldobin/',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  // "deploy": "gh-pages -d out --dotfiles"
+};
+export default nextConfig;
